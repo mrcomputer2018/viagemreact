0ca18144f96b1f18b383641b5c9c9184ef6b08be
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-07-09 18:13:29 -0300

    chore: Update dependencies and add styling to trip planning form

--- README.md
+++ README.md
@@ -28,3 +28,11 @@ export default {
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+## Descrição
+Projeto de planejamento de viagens com envio de e-mail para as pessoas de participaram da viagem. 
+Projeto desenvolvido em react + vite + typescript + tailwindcss
+
+### Bibliotecas
+- tailwindcss - para estelização dos componentes
+- lucide-react - para uso de icones
--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,65 @@
+import { useState } from "react"
+import { MapPin, Calendar, ArrowRight}  from "lucide-react" // Import the IonIcons component from its appropriate module
+
 export function App() {
+    const [destination, setDestination] = useState('')
+
+    const [date, setDate] = useState('')
+
     return (
-        <>
-            <h1>Hello World!!!</h1>
-        </>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <div className="max-w-3xl w-full px-6 text-center rounded-xl space-y-10">
+                <p className="text-zinc-300 text-lg">
+                    Convide seus amigos e planeje sua próxima viagem!
+                </p>
+
+                <div className="h-16 px-4 bg-zinc-900 flex flex-row justify-between items-center shadow-shape">
+                    <div className="flex flex-row justify-center items-center gap-1">
+                        <MapPin className="text-zinc-400 size-5" />
+
+                        <input 
+                        className="placeholder-zinc-400 bg-transparent text-lg"
+                        type="text" 
+                        placeholder="Para onde você vai?"
+                        value={destination}
+                        onChange={(e) => setDestination(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="flex flex-row justify-center items-center gap-1">
+                        <Calendar className="text-zinc-400 size-5" />
+
+                        <input 
+                        className="placeholder-zinc-400 bg-transparent text-lg"
+                        type="text" 
+                        placeholder="Quando?"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="h-full flex flex-row justify-center items-center">
+                        <button 
+                        className="bg-lime-300 text-lime-950 rounded-lg py-2 px-4
+                        flex flex-row items-center justify-center gap-1">
+                            Continuar
+                            <ArrowRight className="text-lime-950 size-5" />
+                        </button>
+                    </div>
+                </div>
+
+                <p className="text-zinc-500 text-sm">
+                    Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>
+                    com nossos &nbsp; 
+                    <a href="#" className="text-zinc-300 underline">
+                        termos de uso
+                    </a> 
+                    &nbsp; e &nbsp; 
+                    <a href="#" className="text-zinc-300 underline">
+                        políticas de privacidade
+                    </a>.
+                </p>
+            </div>
+        </div>
     )
 }
